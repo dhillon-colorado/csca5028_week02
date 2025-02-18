431411207537ac0e343743c1629ffaf942b6f1d3
Amninder Dhillon <amninderdhillon@Mac.attlocal.net>
2025-02-17 21:17:13 -0800

    Added skeleton css

--- src/app.py
+++ src/app.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
+from flask import Flask, request, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='./frontend/templates',static_folder='./frontend/static')
 
 @app.route("/")
 def main():
-    return '''
-     <form action="/echo_user_input" method="POST">
-         <input name="user_input">
-         <input type="submit" value="Submit!">
-     </form>
-     '''
+    return render_template('index.html')
 
-@app.route("/echo_user_input", methods=["POST"])
+    # return '''
+    #  <form action="/echo_user_input" method="POST">
+    #      <input name="user_input">
+    #      <input type="submit" value="Submit!">
+    #  </form>
+    #  '''
+
+@app.route("/echo_user_input", methods=["POST","GET"])
 def echo_input():
-    input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
+    text = request.form.get("user_input", "")
+    return render_template('echo_input.html',input_text=text)
